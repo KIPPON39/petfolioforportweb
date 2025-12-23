b40f860009d43406e983df27a7e02ea4940ec436
Wisit Sriadisak <kurakuri2104@gmail.com>
2025-12-24 02:58:04 +0700

    fix url r2

--- express/petfolio_express/app.js
+++ express/petfolio_express/app.js
@@ -27,7 +27,7 @@ connectDB();
 // CORS ให้ Next.js (3001) เรียกได้
 app.use(
   cors({
-    origin: "http://localhost:3001", // หรือ "*" ชั่วคราว
+    origin: "https://petfolio.wisitdev.com", // หรือ "*" ชั่วคราว
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
